8f82e83cebbe75f6fb644ab769b7e3c610b71621
Martin Egea <mar_egea@hotmail.com>
2023-11-09 16:36:26 -0300

    agregar y eliminar productos del carrito

--- CarritoCompras/carrito.js
+++ CarritoCompras/carrito.js
@@ -14,27 +14,39 @@ const prodCarrito = (producto, precio, cantidad, subTotal, id)=>{
 
 let tablaProductos = document.getElementById('tablaProductos');
 let botonesEliminar = document.querySelectorAll('.botones-eliminar');
+let totalProductos = document.getElementById('totalCarrito');
+
 
 function agregarProdTabla(){
     let listaProductos = "";
-    const productosLS = JSON.parse(localStorage.getItem('productosLS'));
+    const productosLS = JSON.parse(localStorage.getItem('carritoCompras'));
     productosLS.forEach(e => {
-        listaProductos += prodCarrito(e.titulo, e.precio, 1, e.precio, e.id);
+        listaProductos += prodCarrito(e.titulo, e.precio, 1, e.precio, e.id);        
     });
     return listaProductos
+}
+function sumarTotalProductos(){
+    let precioTotal = 0;
+    const productosLS = JSON.parse(localStorage.getItem('carritoCompras'));
+    productosLS.forEach(e => {
+        precioTotal += parseInt(e.precio);        
+    });
+    return precioTotal;
 }
 
 window.addEventListener('load', ()=> {
     tablaProductos.innerHTML = agregarProdTabla()
+    totalProductos.innerHTML = sumarTotalProductos()
 
     actualizarBotonesAgregar()
     botonesEliminar.forEach(boton => {
-        boton.addEventListener('click', (e) =>{
+        boton.addEventListener('click', (e) =>{            
             let idProducto = e.currentTarget.id
             const items = getData('carritoCompras')
             const index = items.findIndex(e => e.id == idProducto)
             items.splice(index,1)
             setData('carritoCompras',items)
+            location. reload()
         })
       })
 })
